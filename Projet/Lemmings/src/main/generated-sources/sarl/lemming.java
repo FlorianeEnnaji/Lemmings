33b8dcdd7607b0c1d2e24e4c821cec798be5f8b2
romain <romthib@caramail.fr>
2016-04-30 18:48:23 +0200

    add a comment to test

---
+++ Projet/Lemmings/src/main/generated-sources/sarl/lemming.java
@@ -0,0 +1,25 @@
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.core.Agent;
+import io.sarl.lang.core.BuiltinCapacitiesProvider;
+import java.util.UUID;
+import javax.annotation.Generated;
+import javax.inject.Inject;
+
+/**
+ * @author romain
+ */
+@SarlSpecification("0.3")
+@SuppressWarnings("all")
+public class lemming extends Agent {
+  /**
+   * Construct an agent.
+   * @param builtinCapacityProvider - provider of the built-in capacities.
+   * @param parentID - identifier of the parent. It is the identifier of the parent agent and the enclosing contect, at the same time.
+   * @param agentID - identifier of the agent. If <code>null</code> the agent identifier will be computed randomly.
+   */
+  @Inject
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  public lemming(final BuiltinCapacitiesProvider builtinCapacityProvider, final UUID parentID, final UUID agentID) {
+    super(builtinCapacityProvider, parentID, agentID);
+  }
+}
