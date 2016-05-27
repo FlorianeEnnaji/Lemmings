df3d8c85cf6083f1a94ed8065e208a27d5b523a3
romain <romthib@caramail.fr>
2016-05-28 01:23:23 +0200

    without error

---
+++ Projet/src/main/generated-sources/sarl/fr/utbm/vi51/lemmings/agent/Simulator.java
@@ -0,0 +1,242 @@
+package fr.utbm.vi51.lemmings.agent;
+
+import fr.utbm.vi51.lemmings.model.Environment;
+import io.sarl.core.AgentKilled;
+import io.sarl.core.AgentSpawned;
+import io.sarl.core.DefaultContextInteractions;
+import io.sarl.core.Destroy;
+import io.sarl.core.Initialize;
+import io.sarl.core.Lifecycle;
+import io.sarl.lang.annotation.EarlyExit;
+import io.sarl.lang.annotation.FiredEvent;
+import io.sarl.lang.annotation.ImportedCapacityFeature;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.core.Address;
+import io.sarl.lang.core.Agent;
+import io.sarl.lang.core.AgentContext;
+import io.sarl.lang.core.BuiltinCapacitiesProvider;
+import io.sarl.lang.core.Event;
+import io.sarl.lang.core.EventSpace;
+import io.sarl.lang.core.Percept;
+import io.sarl.lang.core.Scope;
+import io.sarl.lang.core.Space;
+import io.sarl.lang.core.SpaceID;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.Generated;
+import javax.inject.Inject;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Conversions;
+
+@SarlSpecification("0.3")
+@SuppressWarnings("all")
+public class Simulator extends Agent {
+  protected int waitingAgents = 0;
+  
+  @Percept
+  public void _handle_Initialize_0(final Initialize occurrence) {
+    Object _get = occurrence.parameters[0];
+    Environment environment = ((Environment) _get);
+    int _size = environment.agentBodies.size();
+    int _plus = (_size + 1);
+    this.waitingAgents = _plus;
+    List<Object> agentParameters = CollectionLiterals.<Object>newArrayList(environment);
+    for (int i = 4; (i < ((List<Object>)Conversions.doWrapArray(occurrence.parameters)).size()); i++) {
+      Object _get_1 = occurrence.parameters[i];
+      agentParameters.add(_get_1);
+    }
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#emit(io.sarl.lang.core.Event)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#emit(io.sarl.lang.core.Event)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected void emit(final Event e) {
+    getSkill(io.sarl.core.DefaultContextInteractions.class).emit(e);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#emit(io.sarl.lang.core.Event,io.sarl.lang.core.Scope<io.sarl.lang.core.Address>)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#emit(io.sarl.lang.core.Event,io.sarl.lang.core.Scope<io.sarl.lang.core.Address>)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected void emit(final Event e, final Scope<Address> scope) {
+    getSkill(io.sarl.core.DefaultContextInteractions.class).emit(e, scope);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#getDefaultAddress()}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#getDefaultAddress()
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected Address getDefaultAddress() {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).getDefaultAddress();
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#getDefaultContext()}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#getDefaultContext()
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected AgentContext getDefaultContext() {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).getDefaultContext();
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#getDefaultSpace()}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#getDefaultSpace()
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected EventSpace getDefaultSpace() {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).getDefaultSpace();
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#isDefaultContext(io.sarl.lang.core.AgentContext)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#isDefaultContext(io.sarl.lang.core.AgentContext)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected boolean isDefaultContext(final AgentContext context) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).isDefaultContext(context);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#isDefaultContext(java.util.UUID)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#isDefaultContext(java.util.UUID)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected boolean isDefaultContext(final UUID contextID) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).isDefaultContext(contextID);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#isDefaultSpace(io.sarl.lang.core.Space)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#isDefaultSpace(io.sarl.lang.core.Space)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected boolean isDefaultSpace(final Space space) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).isDefaultSpace(space);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#isDefaultSpace(io.sarl.lang.core.SpaceID)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#isDefaultSpace(io.sarl.lang.core.SpaceID)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected boolean isDefaultSpace(final SpaceID space) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).isDefaultSpace(space);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#isDefaultSpace(java.util.UUID)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#isDefaultSpace(java.util.UUID)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected boolean isDefaultSpace(final UUID space) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).isDefaultSpace(space);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#isInDefaultSpace(io.sarl.lang.core.Event)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#isInDefaultSpace(io.sarl.lang.core.Event)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected boolean isInDefaultSpace(final Event event) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).isInDefaultSpace(event);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#receive(java.util.UUID,io.sarl.lang.core.Event)}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#receive(java.util.UUID,io.sarl.lang.core.Event)
+   */
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected void receive(final UUID receiver, final Event e) {
+    getSkill(io.sarl.core.DefaultContextInteractions.class).receive(receiver, e);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.DefaultContextInteractions#spawn(java.lang.Class<? extends io.sarl.lang.core.Agent>,java.lang.Object[])}.
+   * 
+   * @see io.sarl.core.DefaultContextInteractions#spawn(java.lang.Class<? extends io.sarl.lang.core.Agent>,java.lang.Object[])
+   */
+  @FiredEvent(AgentSpawned.class)
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  protected UUID spawn(final Class<? extends Agent> aAgent, final Object... params) {
+    return getSkill(io.sarl.core.DefaultContextInteractions.class).spawn(aAgent, params);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.Lifecycle#killMe()}.
+   * 
+   * @see io.sarl.core.Lifecycle#killMe()
+   */
+  @EarlyExit
+  @FiredEvent({ AgentKilled.class, Destroy.class })
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(Lifecycle.class)
+  protected void killMe() {
+    getSkill(io.sarl.core.Lifecycle.class).killMe();
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.Lifecycle#spawnInContext(java.lang.Class<? extends io.sarl.lang.core.Agent>,io.sarl.lang.core.AgentContext,java.lang.Object[])}.
+   * 
+   * @see io.sarl.core.Lifecycle#spawnInContext(java.lang.Class<? extends io.sarl.lang.core.Agent>,io.sarl.lang.core.AgentContext,java.lang.Object[])
+   */
+  @FiredEvent(AgentSpawned.class)
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(Lifecycle.class)
+  protected UUID spawnInContext(final Class<? extends Agent> agentClass, final AgentContext context, final Object... params) {
+    return getSkill(io.sarl.core.Lifecycle.class).spawnInContext(agentClass, context, params);
+  }
+  
+  /**
+   * See the capacity {@link io.sarl.core.Lifecycle#spawnInContextWithID(java.lang.Class<? extends io.sarl.lang.core.Agent>,java.util.UUID,io.sarl.lang.core.AgentContext,java.lang.Object[])}.
+   * 
+   * @see io.sarl.core.Lifecycle#spawnInContextWithID(java.lang.Class<? extends io.sarl.lang.core.Agent>,java.util.UUID,io.sarl.lang.core.AgentContext,java.lang.Object[])
+   */
+  @FiredEvent(AgentSpawned.class)
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  @ImportedCapacityFeature(Lifecycle.class)
+  protected UUID spawnInContextWithID(final Class<? extends Agent> agentClass, final UUID agentID, final AgentContext context, final Object... params) {
+    return getSkill(io.sarl.core.Lifecycle.class).spawnInContextWithID(agentClass, agentID, context, params);
+  }
+  
+  /**
+   * Construct an agent.
+   * @param builtinCapacityProvider - provider of the built-in capacities.
+   * @param parentID - identifier of the parent. It is the identifier of the parent agent and the enclosing contect, at the same time.
+   * @param agentID - identifier of the agent. If <code>null</code> the agent identifier will be computed randomly.
+   */
+  @Inject
+  @Generated("io.sarl.lang.jvmmodel.SARLJvmModelInferrer")
+  public Simulator(final BuiltinCapacitiesProvider builtinCapacityProvider, final UUID parentID, final UUID agentID) {
+    super(builtinCapacityProvider, parentID, agentID);
+  }
+}
