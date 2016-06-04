b90af7d8d05fcb63bdc00c2e2c69291d95ae5927
lucillegomez <lucille.alexandra.gomez@gmail.com>
2016-06-05 01:28:07 +0200

    Add javadoc to DynamicObject, LemmingBody, LinkerClass, PerceivableObject, WorldPixel Clean syntaxe

--- Projet/src/main/java/fr/utbm/vi51/lemmings/model/LemmingBody.java
+++ Projet/src/main/java/fr/utbm/vi51/lemmings/model/LemmingBody.java
@@ -1,26 +1,38 @@
 package fr.utbm.vi51.lemmings.model;
 
 import java.awt.Point;
+
 import java.util.List;
 
 import fr.utbm.vi51.lemmings.agent.PerceptionEvent;
 import fr.utbm.vi51.lemmings.utils.enums.ActionEnum;
 import fr.utbm.vi51.lemmings.utils.enums.MoveDirection;
 
+/**
+ * LemmingBody
+ */
 public class LemmingBody extends Body {
 
 	private static final long serialVersionUID = -4636419559142339321L;
 	
 	private List<PerceivableObject> perception;
 	
+	/**
+	 * Constructor
+	 * @param environment the environment the body will be placed in
+	 * @param direction the direction of the body
+	 * @param position the position of the body
+	 */
 	public LemmingBody(Environment environment, MoveDirection direction, Point position) {
 		super(environment, direction, position);
 		LearningRoutine();
 		setPerception(null);
 		learn();
-		
 	}
 
+	/**
+	 * @param influence
+	 */
 	public void influence(Influence influence) {
 		if(influence!=null){
 			if(influence instanceof ActionInfluence){
@@ -53,36 +65,56 @@ public class LemmingBody extends Body {
 		//LearningRoutine();
 	}
 	
+	/**
+	 * @param dir the direction in which the body will walk
+	 */
 	public void walk(MoveDirection dir){
 		move(dir);
 	}
 	
+	/**
+	 * @param dir dir the direction in which the body will dig
+	 */
 	public void dig (MoveDirection dir){
 		Environment e = getEnvironment();
 		e.dig(this, dir);	
 	}
 	
+	/**
+	 * The body climb
+	 */
 	public void climb (){
 		Environment e = getEnvironment();
 		e.climb(this);
 	}
 	
+	/**
+	 * The body jump
+	 */
 	public void jump (){
 		Environment e = getEnvironment();
 		e.jump(this);
 	}
 
+	/**
+	 * @return the serialVersionUID
+	 */
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
+	@Override
 	public List<PerceivableObject> getPerception() {
 		Environment e = getEnvironment();
 		List<PerceivableObject> percept=e.getPerception(this);
 		setPerception(percept);		
-		return perception;
+		return this.perception;
 	}
 
+	/**
+	 * Set the perception
+	 * @param perception the perception to set
+	 */
 	public void setPerception(List<PerceivableObject> perception) {
 		assert(perception!=null);
 		new PerceptionEvent(perception,this);
@@ -99,6 +131,9 @@ public class LemmingBody extends Body {
 		return act;
 	}
 	
+	/**
+	 * Learn function
+	 */
 	public void learn(){
 		Environment e = getEnvironment();
 		int samePosition = 0;
@@ -122,6 +157,4 @@ public class LemmingBody extends Body {
 		}
 	}
 	
-	
-
 }
--- Projet/src/main/java/fr/utbm/vi51/lemmings/model/LinkerClass.java
+++ Projet/src/main/java/fr/utbm/vi51/lemmings/model/LinkerClass.java
@@ -5,23 +5,34 @@ import java.util.TreeMap;
 import java.util.UUID;
 
 import fr.utbm.vi51.lemmings.agent.Lemming;
+
 import io.sarl.core.Initialize;
 
+/**
+ * LinkerClass
+ */
 public class LinkerClass {
 	
 	public final Map<UUID,Lemming> agentMind = new TreeMap<UUID,Lemming>();
     //private janusKernel ja = Boot::startJanus(null,typeof(Lemming),args);
 	
+	/**
+	 * Default Constructor
+	 */
 	public LinkerClass(){
 		
 	}
 	
+	/**
+	 * creation of an agent
+	 * @param ID the id of the agent
+	 */
 	public void createAgent(UUID ID){
 		System.out.println("linker");
 		Lemming agent=new Lemming(null, ID, ID);
 		Initialize occurrence = new Initialize();
 		agent._handle_Initialize_0(occurrence);
-		agentMind.put(ID, agent);
+		this.agentMind.put(ID, agent);
 	}
 
 }
