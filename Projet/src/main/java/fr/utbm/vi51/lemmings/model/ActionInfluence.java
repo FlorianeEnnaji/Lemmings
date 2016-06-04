423688ee6d00d9dc9d2691a7f28e79f93856541a
lucillegomez <lucille.alexandra.gomez@gmail.com>
2016-06-05 01:43:17 +0200

    Add javadoc to ActionInfluence, BehaviourOutput and Body Clean syntaxes

--- Projet/src/main/java/fr/utbm/vi51/lemmings/model/ActionInfluence.java
+++ Projet/src/main/java/fr/utbm/vi51/lemmings/model/ActionInfluence.java
@@ -1,24 +1,29 @@
 package fr.utbm.vi51.lemmings.model;
 
-import java.util.UUID;
-
 import fr.utbm.vi51.lemmings.utils.enums.ActionEnum;
 
+/**
+ * Action Influence
+ */
 public class ActionInfluence extends Influence {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private final ActionEnum action;
 
+	/**
+	 * @param act
+	 */
 	public ActionInfluence(ActionEnum act) {
 		super(null);
 		// TODO Auto-generated constructor stub
 		this.action=act;
 	}
 
+	/**
+	 * @return the type of the action
+	 */
 	public ActionEnum getType(){
-		return action;		
+		return this.action;		
 	}
 	
 
